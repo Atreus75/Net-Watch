d8767526d402e4bb6e6b3accd3d066473ae47a27
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2026-05-23 18:24:59 -0300

    Delete classes.py

--- classes.py
+++
@@ -1,6 +0,0 @@
-class NetEvent:
-    def __init__(self, process, connection):
-        self.process = process
-        self.connection = connection
-        self.occurrences = 1
-        self.regularity = 0
