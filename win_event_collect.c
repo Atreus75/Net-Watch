155f72eb201c45c4df7bb3c3d0589da384b4442b
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2026-05-28 02:35:41 -0300

    Refactor win_event_collect.c for improved event handling
    Refactor event handling and add new features for better event processing.

--- win_event_collect.c
+++ win_event_collect.c
@@ -1,43 +1,31 @@
 #define INITGUID 
+#include "netwatch_lib.h"
 #include <winsock2.h>
 #include <windows.h>
 #include <evntrace.h>
 #include <evntcons.h>
 #include <tdh.h>
-#include <string.h>
 #include <stdio.h>
-
-#define MAX_EVENTS 2
 
 TRACEHANDLE sessionHandle = 0;// Trace session handler
 TRACEHANDLE traceHandle = 0; 
+int EVENT_COUNTER = 0;
+NetEvent * EventCache = NULL;
 
-typedef char IPv4[20];
-typedef struct {
-    DWORD PID;
-    IPv4 source_ip;
-    USHORT source_port;
-    IPv4 dest_ip;
-    USHORT dest_port;
-} NetEvent;
+/* TO-DO:
+    * 1 - Capturar nome do executável envolvido no evento pelo PID
+    * 2 - Implementar 1° check de maliciosidade: verificar se o image do executável se encontra dentre os categorizados como suspeitos em um programs.json simples.
+*/
+void printEvent(NetEvent event){
+    wprintf(L"\n[%hu:%hu:%hu] ", event.moment.hour, event.moment.minute, event.moment.second);
+    if (event.type == CONNECTION_EVENT) wprintf(L"Connection\n");
+    else wprintf(L"Disconnection\n");
+    wprintf(L"    PID: %lu\n", event.PID);
+    wprintf(L"    Source Address: %s:%hu\n", event.source_ip, event.source_port);
+    wprintf(L"    Destination Address: %s:%hu\n", event.dest_ip, event.dest_port);
+}
 
-int SupportedEvents[MAX_EVENTS] = {
-    12,
-    13
-};
-
-// Callback function called by ETW at each event from provider
-VOID WINAPI Callback(PEVENT_RECORD record){
-    UCHAR event_opcode = record->EventHeader.EventDescriptor.Opcode; // Similar to an event ID
-    int suported = 0;
-    // Checks if the event Opcode is supported
-    for (int c = 0; c<MAX_EVENTS; c++){
-        if (SupportedEvents[c] == event_opcode){
-            suported = 1;
-            break;
-        }
-    }
-    if (!suported) return;
+PTRACE_EVENT_INFO getEventMetadata(PEVENT_RECORD record){
     // Getting metadata about the event
     DWORD size = 0;
     TdhGetEventInformation(// Fails and saves the size of PTRACE_EVENT_INFO inside "size"
@@ -57,11 +45,13 @@ VOID WINAPI Callback(PEVENT_RECORD record){
     );
     if (status != ERROR_SUCCESS){
         free(event_metadata);
-        perror("[-] Error ");
-        return;
+        perror("[-] Error: ");
+        return NULL;
     }
+    return event_metadata;
+}
 
-    // Getting the actual data about the event
+NetEvent fillEventStruct(PTRACE_EVENT_INFO event_metadata, PEVENT_RECORD record){
     NetEvent event;
     ZeroMemory(&event, sizeof(event));
     for (ULONG c = 0; c < event_metadata->TopLevelPropertyCount; c++){ // Iterates over the event_metadata properties
@@ -72,28 +62,29 @@ VOID WINAPI Callback(PEVENT_RECORD record){
         ZeroMemory(&desc, sizeof(desc));
         desc.PropertyName = (ULONGLONG)property_name;
         desc.ArrayIndex = ULONG_MAX;
-        DWORD datasize = 0;
-        TdhGetPropertySize(
+        DWORD property_size = 0;
+        TdhGetPropertySize( // Fails and saves the correct descriptor size inside property_size
             record,
             0,
             NULL,
             1,
             &desc,
-            &datasize
+            &property_size
         );
-        BYTE * buffer = (BYTE*)malloc(datasize);
-        status = TdhGetProperty(
+        BYTE * buffer = (BYTE*)malloc(property_size);
+        ULONG status = TdhGetProperty( // Loads the event data inside the buffer
             record,
             0,
             NULL,
             1,
             &desc,
-            datasize,
+            property_size,
             buffer
         );
+
+        // Filling the NetEvent struct for better readability
         USHORT inType = property_info->nonStructType.InType;
         USHORT outType = property_info->nonStructType.OutType;
-        // Filling the NetEvent struct for readability
         switch (inType)
         {
         case 8: //UINT32
@@ -115,21 +106,44 @@ VOID WINAPI Callback(PEVENT_RECORD record){
             break;
         }
         free(buffer);
+        if (strcmp(event.source_ip, event.dest_ip) == 0 && strlen(event.source_ip) > 0){
+            event.type = IRRELEVANT_EVENT;
+            return event; // skips connections from 127.0.0.1 -> 127.0.0.1 and similars
+        } 
     }   
 
-    // Converts timestamp to local time
+    // Converts record time format to local time
     FILETIME filetime;
     FileTimeToLocalFileTime((FILETIME *)(&record->EventHeader.TimeStamp), &filetime);
     SYSTEMTIME systemtime;
     FileTimeToSystemTime(&filetime, &systemtime);
-
-    // Output event information
+    event.moment.hour = (unsigned short)systemtime.wHour;
+    event.moment.minute = (unsigned short)systemtime.wMinute;
+    event.moment.second = (unsigned short)systemtime.wSecond;
+    
     PWSTR eventName = (PWSTR)((PBYTE)event_metadata + event_metadata->OpcodeNameOffset);
+    if (wcscmp(eventName, L"ConnectIPV4") == 0) event.type = CONNECTION_EVENT;
+    else event.type = DISCONNECTION_EVENT;
 
-    wprintf(L"\n[%hu/%hu/%hu][%hu:%hu:%hu] %ls\n", systemtime.wDay, systemtime.wMonth, systemtime.wYear, systemtime.wHour, systemtime.wMinute, systemtime.wSecond, eventName);
-    wprintf(L"    PID: %lu\n", event.PID);
-    wprintf(L"    Source Address: %s:%hu\n", event.source_ip, event.source_port);
-    wprintf(L"    Destination Address: %s:%hu\n", event.dest_ip, event.dest_port);
+    return event;
+}
+
+// Callback function called by ETW at each event from provider
+VOID WINAPI Callback(PEVENT_RECORD record){
+    UCHAR event_opcode = record->EventHeader.EventDescriptor.Opcode; // Similar to an event ID
+    // Checks if the event Opcode is supported
+    if (!isOpcodeSupported(event_opcode)) return;
+
+    // Gets metadata in order to get the actual event data
+    PTRACE_EVENT_INFO event_metadata = getEventMetadata(record);
+    if (!event_metadata) return;
+
+    // Getting the actual data about the event
+    NetEvent event = fillEventStruct(event_metadata, record);
+    if (event.type == IRRELEVANT_EVENT) return;
+    
+    // Output event information
+    printEvent(event);
 }
 
 int main(){
@@ -148,12 +162,12 @@ int main(){
     properties->LoggerNameOffset = sizeof(EVENT_TRACE_PROPERTIES);
     
     // Not necessary, KERNEL_LOGGER is open by default
-    /*ULONG trace_status = StartTrace(
+    ULONG trace_status = StartTrace(
         &sessionHandle,
         KERNEL_LOGGER_NAME,
         properties
     );
-    if (trace_status) wprintf(L"[-] Trace Status: %lu\n", trace_status);*/
+    if (trace_status) wprintf(L"[-] Trace Status: %lu\n", trace_status);
     
     // Opens trace for consuming and defines callback function
     ZeroMemory(&logfile, sizeof(EVENT_TRACE_LOGFILE));
