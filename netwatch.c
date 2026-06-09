8cdb274d7700d6d99af95405123170948c071cf5
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2026-06-09 01:51:02 -0300

    Refactor netwatch.c for event handling and caching

--- netwatch.c
+++ netwatch.c
@@ -1,104 +1,22 @@
-#define INITGUID 
-#include "netwatch_lib.h"
-#include <winsock2.h>
-#include <windows.h>
-#include <evntrace.h>
-#include <evntcons.h>
-#include <tdh.h>
-#include <psapi.h>
-#include <stdio.h>
-#include "cJSON.h"
-
-PWSTR SupportedEvents[MAX_EVENTS] = {
-    (PWSTR)L"DisconnectIPV4",
-    (PWSTR)L"ConnectIPV4",
-    (PWSTR)L"AcceptIPV4"
-};
-
-TRACEHANDLE sessionHandle = 0;// Trace session handler
-TRACEHANDLE traceHandle = 0; 
-int EVENT_COUNTER = 0;
-NetEvent * EventCache = NULL;
-
-
 /* TO-DO:
-    * 1 - Capturar nome do executável envolvido no evento pelo PID
-    * 2 - Implementar 1° check de maliciosidade: verificar se o image do executável se encontra dentre os categorizados como suspeitos em um programs.json simples.
+    * 1 - Salvar eventos em um array de tamanho fixo (ou uma lista encadeada)
+    * 2 - Fazer funções de comparação entre funções (sameImage, sameConnection etc)
+    * 2 - Detectar beaconing utilizando a fila de eventos (Cache)
 */
 
-int suspiciousEventImage(NetEvent * event){
-    FILE * f = fopen("programs.json", "rb");
-    if (!f){
-        printf("[-] Error opening \"programs.json\"\n");
-        return 1;
-    }
-    fseek(f, 0, SEEK_END);
-    long len = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    char * data = malloc(len + 1);
-    fread(data, 1, len, f);
-    data[len] = '\0';
-    fclose(f);
+#include "netwatch_lib.h"
+#include <stdio.h>
 
-    // JSON Parsing
-    cJSON *json = cJSON_Parse(data);
-    free(data);
-    if (!json){
-        printf("[-] Error in JSON parsing.\n");
-        return 1;
-    }
+// Global variables and macros
+#define MAX_CACHE_SIZE 10
+int EVENT_COUNTER = 0;
+NetEventCache Cache = NULL; //Global event cache for event history storing
 
-    // Accessing "Suspicious" array
-    cJSON * suspicious = cJSON_GetObjectItem(json, "Suspicious");
-    if (cJSON_IsArray(suspicious)){
-        for (cJSON * item = suspicious->child; item; item = item->next){
-            if (cJSON_IsString(item)){
-                char * sus_image = item->valuestring;
-                if (strcmp(event->image, sus_image) == 0) return 1;
-            }
-        }
-    }
-    cJSON_Delete(json);
-    return 0;
-}
-
+// Windows functions
+#if defined(_WIN32)
 int isEventSupported(PTRACE_EVENT_INFO event_metadata){
     for (int c = 0; c<MAX_EVENTS; c++) if (wcscmp(SupportedEvents[c], (PWSTR)((PBYTE)event_metadata + event_metadata->OpcodeNameOffset)) == 0) return 1;
     return 0;
-}
-
-void getImageFromPath(NetEvent * event){
-    if (strlen(event->image) == 0) return;
-    int path_size = strlen(event->image), i = 0;
-    char reverted[path_size];
-    ZeroMemory(reverted, path_size);
-    for (int c = path_size-1; c >= 0; c--) reverted[i++] = event->image[c];
-    ZeroMemory(event->image, 260);
-    
-    int first_occurrence = strcspn(reverted, "\\");
-    char rev_image[first_occurrence];
-    ZeroMemory(rev_image, first_occurrence);
-    i = 0;
-    for (int c = 0; c <= first_occurrence; c++) rev_image[i++] = reverted[c];
-    
-    char image[first_occurrence];
-    ZeroMemory(image, first_occurrence+1);
-    i = 0;
-    for (int c = first_occurrence-1; c >= 0; c--) image[i++] = rev_image[c];
-    strcpy(event->image, image);
-}
-
-void printEvent(NetEvent event){
-    if (!suspiciousEventImage(&event)) return;
-    wprintf(L"\n[%hu:%hu:%hu] ", event.moment.hour, event.moment.minute, event.moment.second);
-    if (event.type == OUTGOING_CONNECTION_EVENT) wprintf(L"Outgoing Connection\n");
-    else if (event.type == INCOMMING_CONNECTION_EVENT) wprintf(L"Incomming Connection\n");
-    else wprintf(L"Disconnection\n");
-    wprintf(L"[+] SUSPICIOUS ACTIVITY DETECTED\n");
-    wprintf(L"    PID: %lu\n", event.PID);
-    wprintf(L"    Executable: %s\n", event.image);
-    wprintf(L"    Source Address: %s:%hu\n", event.source_ip, event.source_port);
-    wprintf(L"    Destination Address: %s:%hu\n", event.dest_ip, event.dest_port);
 }
 
 PTRACE_EVENT_INFO getEventMetadata(PEVENT_RECORD record){
@@ -230,8 +148,9 @@ NetEvent fillEventStruct(PTRACE_EVENT_INFO event_metadata, PEVENT_RECORD record)
     return event;
 }
 
-// Callback function called by ETW at each event from provider
+// Windows callback function called by ETW at each event from provider
 VOID WINAPI Callback(PEVENT_RECORD record){
+    signal(SIGINT, terminate);
     // Gets metadata in order to get the actual event data
     PTRACE_EVENT_INFO event_metadata = getEventMetadata(record);
     if (!event_metadata) return;
@@ -241,10 +160,12 @@ VOID WINAPI Callback(PEVENT_RECORD record){
     if (event.type == IRRELEVANT_EVENT) return;
     
     // Output event information
+    if (!suspiciousEvent(&event)) return;
     printEvent(event);
+    saveEvent(event, Cache);
 }
 
-int main(){
+void StartETWTrace(){
     // Trace session configuration
     EVENT_TRACE_PROPERTIES * properties; // Pointer to the strcut thar stores the Trace Session configuration
     EVENT_TRACE_LOGFILE logfile;
@@ -265,7 +186,7 @@ int main(){
         KERNEL_LOGGER_NAME,
         properties
     );
-    if (trace_status) wprintf(L"[-] Trace Status: %lu\n", trace_status);
+    // if (trace_status) wprintf(L"[-] Trace Status: %lu\n", trace_status); Use only for debug
     
     // Opens trace for consuming and defines callback function
     ZeroMemory(&logfile, sizeof(EVENT_TRACE_LOGFILE));
@@ -274,7 +195,165 @@ int main(){
     logfile.EventRecordCallback = Callback;
     traceHandle = OpenTrace(&logfile);
     ProcessTrace(&traceHandle, 1, NULL, NULL);
+    CloseTrace(traceHandle);
+}
 
+#else
+char * SupportedEvents[MAX_EVENTS] = {
+    //insert linux event types here
+}
+#endif 
+
+
+
+// Event Queue functions
+void insertEvent(NetEvent event, NetEventCache queue){
+    if (queue->next == NULL){
+        NetEvent * new = (NetEvent*)malloc(sizeof(NetEvent));
+        *new = event;
+        new->next = NULL;
+        queue->next = new;
+    }else insertEvent(event, queue->next);
+}
+
+void removeEvent(NetEventCache queue){// Always removes the first/older element of the Queue
+    if (queue->next == NULL) return ;
+    NetEvent * next = queue->next->next;
+    free(queue->next);
+    queue->next = next;
+}
+
+void freeQueue(NetEventCache queue){
+    if (queue == NULL) return;
+    freeQueue(queue->next);
+    free(queue);
+}
+
+int saveEvent(NetEvent event, NetEventCache queue){
+    if (EVENT_COUNTER >= MAX_CACHE_SIZE) removeEvent(queue);
+    else EVENT_COUNTER++;
+    insertEvent(event, queue);
+    return 1;
+}
+
+// Event treatment functions
+int suspiciousEventImage(NetEvent * event){
+    FILE * f = fopen("programs.json", "rb");
+    if (!f){
+        printf("[-] Error opening \"programs.json\"\n");
+        return 1;
+    }
+    fseek(f, 0, SEEK_END);
+    long len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char * data = malloc(len + 1);
+    fread(data, 1, len, f);
+    data[len] = '\0';
+    fclose(f);
+
+    // JSON Parsing
+    cJSON *json = cJSON_Parse(data);
+    free(data);
+    if (!json){
+        printf("[-] Error in JSON parsing.\n");
+        return 1;
+    }
+
+    // Accessing "Suspicious" array
+    cJSON * suspicious = cJSON_GetObjectItem(json, "Suspicious");
+    if (cJSON_IsArray(suspicious)){
+        for (cJSON * item = suspicious->child; item; item = item->next){
+            if (cJSON_IsString(item)){
+                char * sus_image = item->valuestring;
+                if (strcmp(event->image, sus_image) == 0) return 1;
+            }
+        }
+    }
+    cJSON_Delete(json);
+    return 0;
+}
+
+int suspiciousEvent(NetEvent * event){
+    return suspiciousEventImage(event);
+}
+
+int sameConnection(NetEvent * evt1, NetEvent * evt2){
+    if (evt1->PID == evt2->PID){
+        if (strcmp(evt1->source_ip, evt2->source_ip) == 0){
+            if (strcmp(evt1->dest_ip, evt2->dest_ip) == 0){
+                if (evt1->dest_port == evt2->dest_port) return 1;
+            }
+        }
+    }
+    
+    return 0;
+}
+
+void getImageFromPath(NetEvent * event){
+    if (strlen(event->image) == 0) return;
+    int path_size = strlen(event->image), i = 0;
+    char reverted[path_size];
+    ZeroMemory(reverted, path_size);
+    for (int c = path_size-1; c >= 0; c--) reverted[i++] = event->image[c];
+    ZeroMemory(event->image, 260);
+    
+    int first_occurrence = strcspn(reverted, "\\");
+    char rev_image[first_occurrence];
+    ZeroMemory(rev_image, first_occurrence);
+    i = 0;
+    for (int c = 0; c <= first_occurrence; c++) rev_image[i++] = reverted[c];
+    
+    char image[first_occurrence];
+    ZeroMemory(image, first_occurrence+1);
+    i = 0;
+    for (int c = first_occurrence-1; c >= 0; c--) image[i++] = rev_image[c];
+    strcpy(event->image, image);
+}
+
+void printEvent(NetEvent event){
+    #if defined(_WIN32)
+    wprintf(L"\n[%hu:%hu:%hu] ", event.moment.hour, event.moment.minute, event.moment.second);
+    if (event.type == OUTGOING_CONNECTION_EVENT) wprintf(L"Outgoing Connection\n");
+    else if (event.type == INCOMMING_CONNECTION_EVENT) wprintf(L"Incomming Connection\n");
+    else wprintf(L"Disconnection\n");
+    wprintf(L"[+] SUSPICIOUS ACTIVITY DETECTED\n");
+    wprintf(L"    PID: %lu\n", event.PID);
+    wprintf(L"    Executable: %s\n", event.image);
+    wprintf(L"    Source Address: %s:%hu\n", event.source_ip, event.source_port);
+    wprintf(L"    Destination Address: %s:%hu\n", event.dest_ip, event.dest_port);
+    #else
+    if (!suspiciousEventImage(&event)) return;
+    printf("\n[%hu:%hu:%hu] ", event.moment.hour, event.moment.minute, event.moment.second);
+    if (event.type == OUTGOING_CONNECTION_EVENT) printf("Outgoing Connection\n");
+    else if (event.type == INCOMMING_CONNECTION_EVENT) printf("Incomming Connection\n");
+    else printf(L"Disconnection\n");
+    printf("[+] SUSPICIOUS ACTIVITY DETECTED\n");
+    printf("    PID: %lu\n", event.PID);
+    printf("    Executable: %s\n", event.image);
+    printf("    Source Address: %s:%hu\n", event.source_ip, event.source_port);
+    printf("    Destination Address: %s:%hu\n", event.dest_ip, event.dest_port);
+    #endif
+}
+
+void terminate(int sig){
+    printf("[-] CTRL+C detected.\n[-] Terminating.\n");
+    if (Cache) freeQueue(Cache);//free Cache if it's not empty
+    exit(0);
+}
+
+int main(){
+    Cache = (NetEventCache)malloc(sizeof(NetEvent));
+    Cache->next = NULL;
+    #if defined(_WIN32)
+
+    wprintf(L"\n==== NETWATCH STARTED ====\n");
+    StartETWTrace();
+
+    #else 
+
+    printf("\n==== NETWATCH STARTED ====\n");
+
+    #endif
     return 0;
 }
 
